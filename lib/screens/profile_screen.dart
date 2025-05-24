9be9264392e0848fdc8f6227f739057f8f4a57d5
deriansibaja <derian.sibaja@vml.com>
2025-05-24 15:58:13 -0600

    feat: adding user profile, name and user

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -14,7 +14,27 @@ class ProfileScreen extends StatelessWidget {
         centerTitle: true,
       ),
       body: const Center(
-        child: Text("Profile Screen", style: TextStyle(fontSize: 24)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            // Profile image
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('https://ui-avatars.com/api/?name=John Doe&background=FF5733&color=fff&rounded=true&size=256'), // Change to your asset path
+            ),
+            SizedBox(height: 16),
+            // Username
+            Text(
+              "John Doe",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            // @account
+            Text(
+              "@johndoe",
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+            ),
+          ],
+        ),
       ),
     );
   }
